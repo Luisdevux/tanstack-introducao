c6f58feab0005578b6dea58e9421c18e02e6d2c9
Luis Felipe <luis.felipe.lopes1275@gmail.com>
2025-10-09 21:35:25 -0400

    Adicionando nova ferramenta de query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import QueryProvider from "@/providers/queryProvider";
 import "./globals.css";
+import { NuqsAdapter } from "nuqs/adapters/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +30,9 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
+          <NuqsAdapter>
           {children}
+          </NuqsAdapter>
         </body>
       </QueryProvider>
     </html>
